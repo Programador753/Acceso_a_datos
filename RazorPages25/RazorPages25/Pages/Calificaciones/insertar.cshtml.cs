49540ffee09946d669323daeade6d55b7075c3b2
“Programador753” <antoniohcavero@outlook.com>
2025-11-20 12:29:05 +0100

    actualizo con insertar

--- RazorPages25/RazorPages25/Pages/Calificaciones/insertar.cshtml.cs
+++ RazorPages25/RazorPages25/Pages/Calificaciones/insertar.cshtml.cs
@@ -18,6 +18,9 @@ namespace RazorPages25.Pages.Calificaciones
         // Repositorio para acceder a los alumnos
         public IAlumnoRepositorio AlumnoRepositorio { get; }
 
+        // Repositorio para acceder a las calificaciones
+        public CalificacionRepositorio CalificacionRepositorio { get; }
+
         #endregion
 
         #region Propiedades de Datos de la Página
@@ -34,6 +37,9 @@ namespace RazorPages25.Pages.Calificaciones
         // Lista de alumnos filtrados por el curso seleccionado
         public List<Alumno> alumnos { get; set; }
 
+        // Lista de calificaciones mostradas
+        public List<Calificacion> CalificacionesMostradas { get; set; }
+
         #endregion
 
         #region Propiedades Enlazadas (BindProperty)
@@ -46,6 +52,10 @@ namespace RazorPages25.Pages.Calificaciones
         [BindProperty(SupportsGet = true)]
         public Calificacion calificacion { get; set; }
 
+        // ID de asignatura seleccionada
+        [BindProperty(SupportsGet = true)]
+        public int asignaturaID { get; set; }
+
         #endregion
 
         #region Constructor
@@ -55,10 +65,12 @@ namespace RazorPages25.Pages.Calificaciones
         /// </summary>
         /// <param name="asignaturaRepositorio">Repositorio de asignaturas</param>
         /// <param name="alumnoRepositorio">Repositorio de alumnos</param>
-        public insertarModel(AsignaturaRepositorio asignaturaRepositorio, IAlumnoRepositorio alumnoRepositorio)
+        /// <param name="calificacionRepositorio">Repositorio de calificaciones</param>
+        public insertarModel(AsignaturaRepositorio asignaturaRepositorio, IAlumnoRepositorio alumnoRepositorio, CalificacionRepositorio calificacionRepositorio)
         {
             AsignaturaRepositorio = asignaturaRepositorio;
             AlumnoRepositorio = alumnoRepositorio;
+            CalificacionRepositorio = calificacionRepositorio;
         }
 
         #endregion
@@ -69,6 +81,37 @@ namespace RazorPages25.Pages.Calificaciones
         /// Manejador GET: carga los datos iniciales para el formulario de inserción
         /// </summary>
         public void OnGet()
+        {
+            CargarDatos();
+        }
+
+        /// <summary>
+        /// Manejador POST: procesa la inserción de una nueva calificación
+        /// </summary>
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                // Re-inicializar datos necesarios para la vista
+                CargarDatos();
+                return Page();
+            }
+
+            CalificacionRepositorio.insertar(calificacion);
+            TempData["mensaje"] = "Calificación insertada correctamente.";
+
+            return RedirectToPage(new
+            {
+                convocatoria = calificacion.convocatoria,
+                curso = curso,
+                asignaturaID = calificacion.asignaturaID
+            }); // Redirige a la misma página con los parámetros necesarios para mostrar las calificaciones actualizadas
+        }
+
+        /// <summary>
+        /// Carga todos los datos necesarios para mostrar el formulario y la tabla de calificaciones
+        /// </summary>
+        private void CargarDatos()
         {
             // Cargar todas las convocatorias disponibles desde el enum
             convocatoria = Enum.GetValues(typeof(Convocatoria)).Cast<Convocatoria>().ToList();
@@ -81,6 +124,28 @@ namespace RazorPages25.Pages.Calificaciones
 
             // Filtrar alumnos según el curso seleccionado
             alumnos = AlumnoRepositorio.GetAlumnosCurso(curso).ToList();
+
+            // Inicializar calificacion si es null
+            if (calificacion == null)
+                calificacion = new Calificacion();
+
+            // Sincronizar asignaturaID entre la propiedad y el objeto calificacion
+            if (calificacion.asignaturaID != 0)
+                asignaturaID = calificacion.asignaturaID;
+            else if (asignaturaID != 0)
+                calificacion.asignaturaID = asignaturaID;
+
+            // Cargar calificaciones filtradas por convocatoria y asignatura
+            if (calificacion.asignaturaID != 0 && calificacion.convocatoria != default(Convocatoria))
+            {
+                CalificacionesMostradas = CalificacionRepositorio
+                    .GetClaCalificacionesConvAsign(calificacion.convocatoria, calificacion.asignaturaID)
+                    .ToList();
+            }
+            else
+            {
+                CalificacionesMostradas = new List<Calificacion>();
+            }
         }
 
         #endregion
