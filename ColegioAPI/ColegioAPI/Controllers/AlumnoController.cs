589465e78fd0da6a38b1f17257207e2a37512aef
“Programador753” <antoniohcavero@outlook.com>
2025-12-16 08:16:53 +0100

    actualizo cambios en la api

--- ColegioAPI/ColegioAPI/Controllers/AlumnoController.cs
+++ ColegioAPI/ColegioAPI/Controllers/AlumnoController.cs
@@ -1,6 +1,7 @@
 ﻿using ColegioAPI.Data;
 using ColegioAPI.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -18,22 +19,48 @@ namespace ColegioAPI.Controllers
 
         // GET: api/<AlumnoController>
         [HttpGet]
-        public ActionResult<IEnumerable<Alumno>> Get()
+        public async Task<ActionResult<IEnumerable<Alumno>>> Get()
         {
-            return Context.Alumnos.ToArray();
+            return await Context.Alumnos.ToListAsync();
         }
 
         // GET api/<AlumnoController>/5
         [HttpGet("{id}")]
-        public ActionResult<Alumno> Get(int id)
+        public async Task<ActionResult<Alumno>> Get(int id)
         {
             return Context.Alumnos.Find(id);
+        }
+
+        // GET api/<AlumnoController>/5
+        [HttpGet("busqueda/{busca}")]
+        public async Task<ActionResult<IEnumerable<Alumno>>> Get(string busca)
+        {
+            return await Context.Alumnos.Where(a => a.Nombre.Contains(busca)).ToListAsync();
+        }
+
+        // POST api/<AlumnoController>
+        [HttpPost]
+        public async Task<ActionResult<Alumno>> Post([FromBody] Alumno alumno)
+        {
+            if (alumno.Id != 0)
+            {
+                var alumnoActualizado = Context.Alumnos.Attach(alumno);
+                alumnoActualizado.State = EntityState.Modified;
+            }
+            else
+            {
+                Context.Alumnos.Add(alumno);
+
+            }
+            await Context.SaveChangesAsync();
+            return alumno;
         }
 
         // POST api/<AlumnoController>
         [HttpPost]
         public void Post([FromBody] string value)
         {
+
         }
 
         // PUT api/<AlumnoController>/5
@@ -44,8 +71,10 @@ namespace ColegioAPI.Controllers
 
         // DELETE api/<AlumnoController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public async Task<ActionResult> Delete(int id)
         {
+            await Context.Database.ExecuteSqlRawAsync("DELETE FROM Alumnos WHERE Id = {0}", id);
+            return Ok(new { mensaje = $"Alumno {id} eliminado" });
         }
     }
 }
--- ColegioAPI/ColegioAPI/obj/Debug/net6.0/ColegioAPI.AssemblyInfo.cs
+++ ColegioAPI/ColegioAPI/obj/Debug/net6.0/ColegioAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ColegioAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b548a699eed0a63522d24ea1d2fb3d208f36bd34")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da8f5d9eaaf5bdb35388b8fa323efe884543808b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ColegioAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ColegioAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
