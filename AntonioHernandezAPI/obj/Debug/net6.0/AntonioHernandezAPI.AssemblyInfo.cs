6d44abc11354ea081b96d16ba279c0d9a6bc32b8
“Programador753” <antoniohcavero@outlook.com>
2026-03-04 12:50:36 +0100

    actualizo

--- AntonioHernandezAPI/obj/Debug/net6.0/AntonioHernandezAPI.AssemblyInfo.cs
+++ AntonioHernandezAPI/obj/Debug/net6.0/AntonioHernandezAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AntonioHernandezAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a323fcb5b6deffdafa1ced45303243b62dfaad87")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+159c7ccf086ed04be34e9c0b6368d3c186d466b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("AntonioHernandezAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AntonioHernandezAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- RazorPages25/RazorPages.Services/obj/Debug/net6.0/RazorPages.Services.AssemblyInfo.cs
+++ RazorPages25/RazorPages.Services/obj/Debug/net6.0/RazorPages.Services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RazorPages.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fc6e3c5342f42f4efa7ea9a5ad050644a954905")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+159c7ccf086ed04be34e9c0b6368d3c186d466b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("RazorPages.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RazorPages.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
